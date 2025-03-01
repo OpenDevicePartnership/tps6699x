8f992923fb838c7933af7c8bb1a07dde08cb47f0
Robert Zieba <robertzieba@microsoft.com>
2025-03-01 13:47:09 -0700

    asynchronous: Implement source enablement functions

--- src/asynchronous/embassy/mod.rs
+++ src/asynchronous/embassy/mod.rs
@@ -153,6 +153,24 @@ impl<'a, M: RawMutex, B: I2c> Tps6699x<'a, M, B> {
         self.lock_inner().await.get_port_control(port).await
     }
 
+    /// Wrapper for `get_system_config`
+    pub async fn get_system_config(&mut self) -> Result<registers::field_sets::SystemConfig, Error<B::Error>> {
+        self.lock_inner().await.get_system_config().await
+    }
+
+    /// Wrapper for `set_system_config`
+    pub async fn set_system_config(
+        &mut self,
+        config: registers::field_sets::SystemConfig,
+    ) -> Result<(), Error<B::Error>> {
+        self.lock_inner().await.set_system_config(config).await
+    }
+
+    /// Wrapper for `enable_source`
+    pub async fn enable_source(&mut self, port: PortId, enable: bool) -> Result<(), Error<B::Error>> {
+        self.lock_inner().await.enable_source(port, enable).await
+    }
+
     /// Returns the number of ports
     pub fn num_ports(&self) -> usize {
         self.controller.num_ports
