ff2d19dfcc6641e0d33cce0fac9c9649e097fa93
Adam Sasine <asasine@users.noreply.github.com>
2026-04-09 11:18:54 -0700

    Add Received SOP Prime Identity Data register

---
+++ src/registers/received_sop_prime_identity_data.rs
@@ -0,0 +1,141 @@
+//! Received SOP Prime Identity Data Object register (`0x49`).
+//!
+//! This register's size exceeds the maximum supported length by the [`device_driver`] crate.
+//!
+//! This register contains the response to Discover Identity command sent to the SOP' or SOP'' cable plug.
+
+use bitfield::bitfield;
+use embedded_usb_pd::vdm::structured::{
+    command::discover_identity::{
+        sop_prime::{id_header_vdo, IdHeaderVdo},
+        CertStatVdo, ProductTypeVdo, ProductVdo,
+    },
+    header::CommandType,
+};
+
+use crate::debug;
+
+/// The address of the `Received SOP Prime Identity Data Object` register.
+pub const ADDR: u8 = 0x49;
+
+/// The length of the `Received SOP Prime Identity Data Object` register, in bytes.
+///
+/// This exceeds the maximum supported length by the [`device_driver`] crate.
+pub const LEN: usize = 200 / 8;
+
+bitfield! {
+    /// Received source/sink capabilities register
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
+    struct Raw([u8]);
+    impl Debug;
+
+    /// Number of valid VDOs in this register (max of 6).
+    pub u8, number_valid_vdos, set_number_valid_vdos: 2, 0;
+
+    /// Type of response received.
+    ///
+    /// See [`ResponseType`].
+    pub u8, response_type, set_response_type: 7, 6;
+
+    pub u32, vdo1, set_vdo1: 39, 8;
+    pub u32, vdo2, set_vdo2: 71, 40;
+    pub u32, vdo3, set_vdo3: 103, 72;
+    pub u32, vdo4, set_vdo4: 135, 104;
+    pub u32, vdo5, set_vdo5: 167, 136;
+    pub u32, vdo6, set_vdo6: 199, 168;
+}
+
+/// Received SOP Prime Identity Data Object register, containing the identity information returned from `Discover Identity REQ` messages.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct ReceivedSopPrimeIdentityData(Raw<[u8; LEN]>);
+
+impl ReceivedSopPrimeIdentityData {
+    pub const DEFAULT: Self = Self(Raw([0; LEN]));
+
+    /// Returns the number of valid VDOs in this register.
+    pub fn number_valid_vdos(&self) -> usize {
+        self.0.number_valid_vdos() as usize
+    }
+
+    /// Returns an iterator over the VDOs.
+    ///
+    /// Each response usually contains an ID Header VDO, a Cert Stat VDO, a Product VDO,
+    /// and up to 3 Product Type VDOs whose types are context-specific. Specific
+    /// methods are available to parse the first 3 VDOs and to retrieve the
+    /// Product Type VDOs.
+    ///
+    /// - ID Header VDO: [`Self::id_header`]
+    /// - Cert Stat VDO: [`Self::cert_stat`]
+    /// - Product VDO: [`Self::product_vdo`]
+    /// - Product Type VDOs: [`Self::product_type_vdos`]
+    pub fn vdos(&self) -> impl ExactSizeIterator<Item = u32> {
+        [
+            self.0.vdo1(),
+            self.0.vdo2(),
+            self.0.vdo3(),
+            self.0.vdo4(),
+            self.0.vdo5(),
+            self.0.vdo6(),
+        ]
+        .into_iter()
+        .take(self.number_valid_vdos())
+    }
+
+    /// The type of response received for the Discover Identity command sent to
+    /// the SOP' or SOP'' cable plug.
+    ///
+    /// See [`CommandType`] for more details.
+    pub fn response_type(&self) -> CommandType {
+        self.0.response_type().into()
+    }
+
+    /// Contains information corresponding to the Power Delivery Product.
+    ///
+    /// Returns [`None`] if there isn't enough valid VDOs to contain an ID Header VDO.
+    /// If there are, attempts to parse it as an [`IdHeaderVdo`] and returns the result.
+    /// If that fails, returns the raw VDO for further analysis.
+    pub fn id_header(&self) -> Option<Result<IdHeaderVdo, id_header_vdo::Raw>> {
+        let raw = self.vdos().next()?;
+        let raw = id_header_vdo::Raw(raw);
+        match IdHeaderVdo::try_from(raw) {
+            Ok(id_header) => Some(Ok(id_header)),
+            Err(e) => {
+                debug!("Failed to parse ID Header VDO: {:?}", e);
+                Some(Err(raw))
+            }
+        }
+    }
+
+    /// Contains the XID assigned by USB-IF to the product before certification,
+    /// in binary format.
+    pub fn cert_stat(&self) -> Option<CertStatVdo> {
+        self.vdos().nth(1).map(CertStatVdo)
+    }
+
+    /// Contains identity information relating to the product.
+    pub fn product_vdo(&self) -> Option<ProductVdo> {
+        self.vdos().nth(2).map(ProductVdo::from)
+    }
+
+    /// Return an iterator over the Product Type VDOs, if present.
+    ///
+    /// The interpretation of these VDOs is context-specific based on the contents
+    /// of the [`Self::id_header`]. Some or all may be padding with the value of `0x00000000`.
+    pub fn product_type_vdos(&self) -> impl Iterator<Item = ProductTypeVdo> {
+        self.vdos().skip(3).map(ProductTypeVdo)
+    }
+}
+
+impl Default for ReceivedSopPrimeIdentityData {
+    fn default() -> Self {
+        Self::DEFAULT
+    }
+}
+
+impl From<[u8; LEN]> for ReceivedSopPrimeIdentityData {
+    fn from(raw: [u8; LEN]) -> Self {
+        Self(Raw(raw))
+    }
+}
