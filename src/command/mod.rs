879a7bd48b9eef0fae49acc2adb4eba298374a2e
Adam Sasine <asasine@users.noreply.github.com>
2026-09-14 16:16:05 -0700

    Change test assertion to a more useful invariant

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -627,9 +627,12 @@ mod test {
 
     #[test]
     fn test_tfuc_timeout_reserves_independent_verification_slack() {
-        assert_eq!(Command::Tfuc.timeout_ms(), RESET_DELAY_MS + TFUC_VERIFICATION_SLACK_MS);
-        assert_eq!(TFUC_VERIFICATION_SLACK_MS, 500);
-        assert_eq!(Command::Gaid.timeout_ms(), RESET_DELAY_MS + 100);
+        let actual = Command::Tfuc.timeout_ms();
+        assert!(
+            actual > 2 * RESET_DELAY_MS,
+            "TFUc timeout ({} ms) should be longer than internal delay of 2 * RESET_DELAY_MS",
+            actual
+        );
     }
 
     #[test]
